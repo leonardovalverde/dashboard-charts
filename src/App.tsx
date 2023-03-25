72cc2c2ef97c4cc54292c585c3ce50da5987d0a5
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-25 11:52:47 -0300

    feat: inject antd provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { ConfigProvider } from "antd";
+import ptBR from "antd/lib/locale/pt_BR";
+
 const App = (): JSX.Element => {
-  return <div className="App">é isto</div>;
+  return (
+    <ConfigProvider locale={ptBR}>
+      <div className="App">é isto</div>
+    </ConfigProvider>
+  );
 };
 
 export default App;
