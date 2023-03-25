0b2d43f68ff58774c5326066276de8aa452b184f
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-25 04:54:16 -0300

    feat: create types for assets req

---
+++ src/services/assets/types.ts
@@ -0,0 +1,32 @@
+export interface IHealthHistory {
+  [index: number]: {
+    status: string;
+    timestamp: string;
+  };
+}
+
+export interface ISpecifications {
+    maxTemp: number;
+    power?: number;
+    rpm?: number | null;
+}
+
+export interface IAsset {
+  assignedUserIds: number[];
+  companyId: number;
+  healthHistory: IHealthHistory;
+  healthscore: number;
+  id: number;
+  image: string;
+  metrics: {
+    lastUptimeAt: string;
+    totalCollectsUptime: number;
+    totalUptime: number;
+  };
+  model: string;
+  name: string;
+  sensors: string[];
+  specifications: ISpecifications;
+  status: string;
+  unitId: number;
+}
