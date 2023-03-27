fa1bed553c4adfc6fcc8c2b41aa473f5440d030c
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-27 01:08:20 -0300

    feat: include new types

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,25 +1,26 @@
 export const endpoints = {
   assets: {
     getAll: `${process.env.REACT_APP_API_BASE_URL}assets`,
-    getById: (id: string) =>
+    getById: (id: string | number) =>
       `${process.env.REACT_APP_API_BASE_URL}assets/${id}`,
   },
   users: {
     getAll: `${process.env.REACT_APP_API_BASE_URL}users`,
-    getById: (id: string) => `${process.env.REACT_APP_API_BASE_URL}users/${id}`,
+    getById: (id: string | number) => `${process.env.REACT_APP_API_BASE_URL}users/${id}`,
   },
   units: {
     getAll: `${process.env.REACT_APP_API_BASE_URL}units`,
-    getById: (id: string) => `${process.env.REACT_APP_API_BASE_URL}units/${id}`,
+    getById: (id: string | number) =>
+      `${process.env.REACT_APP_API_BASE_URL}units/${id}`,
   },
   companies: {
     getAll: `${process.env.REACT_APP_API_BASE_URL}companies`,
-    getById: (id: string) =>
+    getById: (id: string | number) =>
       `${process.env.REACT_APP_API_BASE_URL}companies/${id}`,
   },
   workOrders: {
     getAll: `${process.env.REACT_APP_API_BASE_URL}workOrders`,
-    getById: (id: string) =>
+    getById: (id: string | number) =>
       `${process.env.REACT_APP_API_BASE_URL}workOrders/${id}`,
   },
 };
