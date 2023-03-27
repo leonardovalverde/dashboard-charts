6e988025c7a187ded72842ea2a41071a8c2e880f
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-27 01:08:05 -0300

    feat: include new types

--- src/services/units/units.ts
+++ src/services/units/units.ts
@@ -7,7 +7,7 @@ export const getUnits = async (): Promise<IUnit[]> => {
   return response.data;
 };
 
-export const getUnitById = async (id: string): Promise<IUnit> => {
+export const getUnitById = async (id: string | number): Promise<IUnit> => {
   const response = await axios.get(endpoints.units.getById(id));
   return response.data;
 };
--- src/services/users/types.ts
+++ src/services/users/types.ts
@@ -1,8 +1,13 @@
+import { IUnit } from "services/units/types";
+import { ICompany } from "services/companies/types";
+
 export interface IUser {
   companyId: number;
   email: string;
   id: number;
   name: string;
-  unitId: number;
+  unitId: number | string;
+  company: ICompany;
+  unit: IUnit;
   isLogged?: boolean;
 }
