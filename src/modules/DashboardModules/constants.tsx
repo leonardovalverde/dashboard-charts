2af41a481fea0758d11b6f36b126c2bcd5ffbea1
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-27 01:12:52 -0300

    feat: create constant file for dashboard modules main component

--- src/modules/DashboardModules/constants.tsx
+++ src/modules/DashboardModules/constants.tsx
@@ -3,16 +3,19 @@ import AssetsModule from "./AssetsModule/index";
 import CompaniesModule from "./CompaniesModule/index";
 import UnitsModule from "./UnitsModule/index";
 import UsersModule from "./UsersModule/index";
+import { IUser } from "services/users/types";
 interface IModules {
   [key: string]: JSX.Element;
 }
 
-const modules: IModules = {
-  home: <HomeModule />,
-  assets: <AssetsModule />,
-  companies: <CompaniesModule />,
-  units: <UnitsModule />,
-  users: <UsersModule />,
+const modules = (userData: IUser): IModules => {
+  return {
+    home: <HomeModule userData={userData} />,
+    assets: <AssetsModule />,
+    companies: <CompaniesModule />,
+    units: <UnitsModule />,
+    users: <UsersModule />,
+  };
 };
 
 export { modules };
