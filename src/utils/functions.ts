a95afd4bde351f3088cd2015b393e8525f40e32e
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-27 01:07:23 -0300

    feat: create function to get menu items

---
+++ src/utils/functions.ts
@@ -0,0 +1,21 @@
+import { MenuProps } from "antd";
+
+export type MenuItem = Required<MenuProps>["items"][number];
+
+const getItem = (
+  label: React.ReactNode,
+  key?: React.Key | null,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: "group"
+): MenuItem => {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+};
+
+export { getItem };
