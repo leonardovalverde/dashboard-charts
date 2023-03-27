4d58992efe7442dfb37989609180a9cb12aed4cd
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-27 01:12:24 -0300

    feat: improve dashboard main component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,31 @@
 import { MenuProps } from "antd";
 import { useState } from "react";
-import DashboardModulesRender from "../../modules/DashboardModules";
-import DashboardHeader from "./components/DashbaordHeader/DashboardHeader";
+import { useSelector } from "react-redux";
+import { IUserState } from "store/slice/userSlice";
+import DashboardModulesRender from "modules/DashboardModules";
+import DashboardHeader from "./components/DashboardHeader/DashboardHeader";
+import DashboardMenu from "./components/DashboardMenu/DashboardMenu";
+import { Container } from "./styles";
 
 const Dashboard = (): JSX.Element => {
+  const userData = useSelector((state: IUserState) => state.user);
   const [current, setCurrent] = useState<string>("home");
 
-  const handleSelected: MenuProps["onClick"] = (e) => {
-    setCurrent(e.key);
+  const handleSelected: MenuProps["onClick"] = (active) => {
+    setCurrent(active.key);
   };
 
   return (
     <>
-      <DashboardHeader onClick={handleSelected} current={current} />
-      <DashboardModulesRender current={current} />
+      <DashboardHeader userData={userData} />
+      <Container>
+        <DashboardMenu
+          current={current}
+          onClick={handleSelected}
+          userData={userData}
+        />
+        <DashboardModulesRender current={current} userData={userData} />
+      </Container>
     </>
   );
 };
---
+++ src/pages/Dashboard/styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  position: relative;
+  height: calc(100vh - 60px);
+`;
+
+export { Container };
