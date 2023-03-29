666b50a70b5a9c77cf9e60ede3b6b10826288607
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-29 00:33:06 -0300

    feat: adapt hook to use rtk query

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,87 +1,46 @@
-import { isAxiosError } from "axios";
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState } from "react";
 import { useDispatch } from "react-redux/es/exports";
 import { useNavigate } from "react-router-dom";
-import { getUsers } from "../services/users/users";
 import { setUser } from "../store/slice/userSlice";
 import { IUseAuth } from "./types";
-import { IUser } from "services/users/types";
-import { getCompanyById } from "services/companies/companies";
-import { getUnitById } from "../services/units/units";
+import { useGetAllUsersQuery } from "services/users/users";
 
 const useAuth = (): IUseAuth => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(false);
-  const [userData, setUserData] = useState<IUser>({} as IUser);
   const [error, setError] = useState<string>("");
+  const { data, isLoading } = useGetAllUsersQuery();
 
-  const signIn = useCallback(async (email: string) => {
-    setIsLoading(true);
-    try {
-      const data = await getUsers();
-      const user = data.find((user) => user.email === email);
-      if (!user) {
-        setError("Usuário não encontrado");
+  const signIn = useCallback(
+    async (email: string) => {
+      if (data) {
+        setError("");
+        const user = data.find((user) => user.email === email);
+        if (user) {
+          setError("");
+          dispatch(
+            setUser({
+              ...user,
+              isLogged: true,
+            })
+          );
+          navigate("/dashboard");
+        } else {
+          setError("Usuário não encontrado");
+        }
       } else {
-        setUserData(user);
+        setError(
+          "Erro ao buscar usuários, por favor tente novamente mais tarde"
+        );
       }
-    } catch (error) {
-      if (isAxiosError(error)) {
-        setError(error.message);
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
+    },
+    [data, dispatch, navigate]
+  );
 
   const signOut = useCallback(() => {
     dispatch(setUser({ isLogged: false }));
     navigate("/");
   }, [dispatch, navigate]);
-
-  const getUserCompany = useCallback(async (companyId: string | number) => {
-    try {
-      const company = await getCompanyById(companyId);
-      return company;
-    } catch (error) {
-      if (isAxiosError(error)) {
-        setError(error.message);
-      }
-    }
-  }, []);
-
-  const getUserUnit = useCallback(
-    async (unitId: string | number) => {
-      try {
-        const unit = await getUnitById(unitId);
-        navigate("/dashboard");
-        return unit;
-      } catch (error) {
-        if (isAxiosError(error)) {
-          setError(error.message);
-        }
-      }
-    },
-    [navigate]
-  );
-
-  useEffect(() => {
-    const fetchUserDetails = async () => {
-      if (userData.id) {
-        dispatch(
-          setUser({
-            ...userData,
-            isLogged: true,
-            company: await getUserCompany(userData.companyId),
-            unit: await getUserUnit(userData.unitId),
-          })
-        );
-      }
-    };
-
-    fetchUserDetails();
-  }, [userData, dispatch, getUserCompany, getUserUnit]);
 
   return { signIn, error, isLoading, signOut };
 };
