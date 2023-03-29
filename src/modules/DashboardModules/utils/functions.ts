4bbdd3feaa0334c80a17be368da76ceb14fa846e
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-29 00:39:44 -0300

    feat: create functions to filter data

--- src/modules/DashboardModules/utils/functions.ts
+++ src/modules/DashboardModules/utils/functions.ts
@@ -1,23 +1,18 @@
 import { IAsset } from "services/assets/types";
 import { red, green, yellow, orange, gray } from "@ant-design/colors";
+import { IUser } from "../../../services/users/types";
+import { IWorkOrder } from "services/workOrders/types";
 
 const getOnlyAsignedAssets = (assets: IAsset[], userId: number): IAsset[] => {
-  const assetsByUser = assets.filter(
-    (asset) => asset.assignedUserIds.filter((id) => id === userId).length > 0
+  const assetsByUser = assets.filter((asset) =>
+    asset.assignedUserIds.includes(userId)
   );
   return assetsByUser;
 };
 
 const orderAssetsByScore = (assets: IAsset[]): IAsset[] => {
-  const orderedAssets = assets.sort((a, b) => {
-    if (a.healthscore > b.healthscore) {
-      return -1;
-    } else if (a.healthscore < b.healthscore) {
-      return 1;
-    } else {
-      return 0;
-    }
-  });
+  const assetsArray = [...assets]
+  const orderedAssets = assetsArray.sort((a, b) => b.healthscore - a.healthscore);
   return orderedAssets;
 };
 
@@ -25,9 +20,9 @@ const getColorByScore = (score: number): string => {
   if (score >= 0 && score <= 40) {
     return red[4];
   } else if (score > 40 && score <= 60) {
-    return orange[5];
+    return orange[4];
   } else if (score > 60 && score <= 70) {
-    return yellow[4];
+    return yellow[5];
   } else if (score > 70 && score <= 100) {
     return green[4];
   } else {
@@ -46,6 +41,28 @@ const getColorByStatus = (status: string): string => {
     return red[4];
   } else if (status === "plannedStop") {
     return gray[4];
+  } else {
+    return gray[0];
+  }
+};
+
+const getColorByPriority = (priority: string): string => {
+  if (priority === "high") {
+    return red[4];
+  } else if (priority === "medium") {
+    return orange[4];
+  } else if (priority === "low") {
+    return green[5];
+  } else {
+    return gray[0];
+  }
+};
+
+const getColorByProgress = (progress: string): string => {
+  if (progress === "in progress") {
+    return orange[4];
+  } else if (progress === "completed") {
+    return green[4];
   } else {
     return gray[0];
   }
@@ -83,11 +100,41 @@ const getAssetsNameByStatus = (
   return assetsNamesByStatus;
 };
 
+const getUsersOfSameCompany = (companyId: number, users: IUser[]): IUser[] => {
+  const usersOfSameCompany = users.filter(
+    (user) => user.companyId === companyId
+  );
+  return usersOfSameCompany;
+};
+
+const getWorkOrdersByAssignedUserId = (
+  userId: number,
+  workOrders: IWorkOrder[]
+): IWorkOrder[] => {
+  const workOrdersByAssignedUserId = workOrders.filter((workOrder) =>
+    workOrder.assignedUserIds.includes(userId)
+  );
+  return workOrdersByAssignedUserId;
+};
+
+const createCheckListArray = (object: any): any[] => {
+  const arrayOfObjects = Object.keys(object).map((key) => ({
+    completed: object[key],
+    task: key,
+  }));
+  return arrayOfObjects;
+};
+
 export {
   getOnlyAsignedAssets,
   orderAssetsByScore,
   getColorByScore,
   getColorByStatus,
+  getColorByPriority,
+  getColorByProgress,
   countAssetsWithStatus,
   getAssetsNameByStatus,
+  getUsersOfSameCompany,
+  getWorkOrdersByAssignedUserId,
+  createCheckListArray,
 };
