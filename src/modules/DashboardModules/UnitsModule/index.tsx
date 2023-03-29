b90ff97bbc8eafe284724fc68cd4d1b1ea92cdcd
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-29 00:37:16 -0300

    feat: create module to show units info

--- src/modules/DashboardModules/UnitsModule/index.tsx
+++ src/modules/DashboardModules/UnitsModule/index.tsx
@@ -1,7 +1,52 @@
+import { useGetAllUnitsQuery } from "services/units/units";
+import { ColumnsType } from "antd/es/table";
+import { Container, LoadingWrapper } from "./styles";
+import SortingTable from "components/Table/SortingTable/SortingTable";
+import { Spin } from "antd";
+import { blue } from '@ant-design/colors';
+
 const UnitsModule = (): JSX.Element => {
+  const { data, isLoading, error } = useGetAllUnitsQuery();
+
+  const columns: ColumnsType<any> = [
+    {
+      title: "id",
+      dataIndex: "id",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => a.id - b.id,
+    },
+    {
+      title: "Nome",
+      dataIndex: "name",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => a.name.toString().localeCompare(b.name.toString()),
+    },
+  ];
+
+  const unitsData =
+    data?.map((unit) => {
+      return {
+        key: unit.id,
+        id: unit.id,
+        name: unit.name,
+      };
+    }) || [];
+
   return (
     <>
-      <h1>UnitsModule</h1>
+      {isLoading ? (
+        <LoadingWrapper>
+          <Spin />
+        </LoadingWrapper>
+      ) : (
+        <Container>
+          <SortingTable
+            data={unitsData}
+            columns={columns}
+            pagination={{ pageSize: 20, position: ["topCenter"], style: {backgroundColor: blue[0], margin: 0, padding: "10px 0"} }}
+          />
+        </Container>
+      )}
     </>
   );
 };
