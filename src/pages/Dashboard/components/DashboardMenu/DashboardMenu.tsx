e26cb7b665f11b31e9d714ec121d1371a06a213c
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-29 00:34:38 -0300

    feat: include admin mode and redirects

--- src/pages/Dashboard/components/DashboardMenu/DashboardMenu.tsx
+++ src/pages/Dashboard/components/DashboardMenu/DashboardMenu.tsx
@@ -1,22 +1,30 @@
 import {
   AppstoreOutlined,
-  MailOutlined,
+  AreaChartOutlined,
+  DeploymentUnitOutlined,
   SettingOutlined,
+  UserOutlined,
 } from "@ant-design/icons";
-import { Menu } from "antd";
+import { Menu, Spin, Switch } from "antd";
 import Paragraph from "components/Typography/Paragraph";
 import { getItem, MenuItem } from "utils/functions";
-import { CompanyContainer, StyledSider } from "./styles";
+import { CompanyContainer, StyledSider, ToggleAdminContainer } from "./styles";
 import { DashboardMenuProps } from "./types";
+import { useGetUnitByIdQuery } from "services/units/units";
+import { useGetCompanyByIdQuery } from "services/companies/companies";
+import { setUser } from "store/slice/userSlice";
+import { useDispatch } from "react-redux/es/exports";
 
 const items: MenuItem[] = [
-  getItem("Início", "home", <MailOutlined />),
+  getItem("Início", "home", <AreaChartOutlined />),
+
+  getItem("Ordens de serviço", "workOrders", <SettingOutlined />),
 
   getItem("Ativos", "assets", <AppstoreOutlined />),
 
-  getItem("Usuários", "users", <SettingOutlined />),
+  getItem("Usuários", "users", <UserOutlined />),
 
-  getItem("Unidades", "units", <SettingOutlined />),
+  getItem("Unidades", "units", <DeploymentUnitOutlined />),
 ];
 
 const DashboardMenu = ({
@@ -24,15 +32,39 @@ const DashboardMenu = ({
   current,
   userData,
 }: DashboardMenuProps): JSX.Element => {
+  const { data, isLoading, error } = useGetUnitByIdQuery(userData.unitId);
+  const {
+    data: companyData,
+    isLoading: companyIsLoading,
+    error: companyError,
+  } = useGetCompanyByIdQuery(userData.unitId);
+  const dispatch = useDispatch();
+
+  const handleAdminMode = (value: boolean) => {
+    if (value && value === true) {
+      dispatch(setUser({ ...userData, isAdmin: true }));
+    } else {
+      dispatch(setUser({ ...userData, isAdmin: false }));
+    }
+  };
+
   return (
     <StyledSider>
       <CompanyContainer>
-        <Paragraph color="#fff" strong>
-          {userData.company.name}
-        </Paragraph>
-        <Paragraph color="#fff" strong>
-          {userData.unit.name}
-        </Paragraph>
+        {isLoading || companyIsLoading ? (
+          <Spin />
+        ) : (
+          <>
+            <Paragraph color="#fff" strong>
+              {companyError
+                ? "Erro ao carregar o nome da empresa"
+                : companyData?.name}
+            </Paragraph>
+            <Paragraph color="#fff" strong>
+              {error ? "Erro ao carregar o nome da unidade" : data?.name}
+            </Paragraph>
+          </>
+        )}
       </CompanyContainer>
       <Menu
         theme="dark"
@@ -45,6 +77,14 @@ const DashboardMenu = ({
         mode="inline"
         items={items}
       />
+      <ToggleAdminContainer>
+        <Switch
+          checkedChildren="Modo Admin"
+          unCheckedChildren="Modo Usuário"
+          onChange={(value) => handleAdminMode(value)}
+          defaultChecked={userData.isAdmin}
+        />
+      </ToggleAdminContainer>
     </StyledSider>
   );
 };
