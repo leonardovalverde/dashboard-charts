016e656b470f76747a4a2c71838c56e7a59b2043
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-30 01:12:01 -0300

    style: create some reponsivity to table

--- src/components/Table/SortingTable/styles.tsx
+++ src/components/Table/SortingTable/styles.tsx
@@ -2,6 +2,8 @@ import { Table } from "antd";
 import styled from "styled-components";
 
 const StyledTable = styled(Table)`
+    width: 100%;
+    overflow: auto; 
     .hidden {
         display: none;P 
     }
