bd00f52c9e245f9af2643f1a3a9cc77263fbca5e
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-30 00:06:55 -0300

    feat: insert action component and delete logic

--- src/modules/DashboardModules/UnitsModule/index.tsx
+++ src/modules/DashboardModules/UnitsModule/index.tsx
@@ -1,14 +1,28 @@
-import { Spin } from "antd";
+import { useEffect, useState } from "react";
+import { Button, Spin } from "antd";
 import { type ColumnsType } from "antd/es/table";
 import SortingTable from "components/Table/SortingTable/SortingTable";
-import { useGetAllUnitsQuery } from "services/units/units";
+import { type IUnit } from "services/units/types";
+import {
+  useDeleteUnityByIdMutation,
+  useGetAllUnitsQuery,
+} from "services/units/units";
 
-import { blue } from '@ant-design/colors';
+import { blue } from "@ant-design/colors";
 
+import ActionHeader from "./components/ActionHeader/ActionHeader";
 import { Container, LoadingWrapper } from "./styles";
+import { type UnitsModuleProps } from "./type";
 
-const UnitsModule = (): JSX.Element => {
+const UnitsModule = ({ userData }: UnitsModuleProps): JSX.Element => {
+  const [units, setUnits] = useState<IUnit[]>([]);
+  const [updatePost] = useDeleteUnityByIdMutation();
   const { data, isLoading } = useGetAllUnitsQuery();
+
+  const handleDelete = (key: string): void => {
+    void updatePost(key).unwrap();
+    setUnits((prev) => prev.filter((item) => item.id !== parseInt(key)));
+  };
 
   const columns: ColumnsType<any> = [
     {
@@ -23,16 +37,38 @@ const UnitsModule = (): JSX.Element => {
       defaultSortOrder: "descend",
       sorter: (a, b) => a.name.toString().localeCompare(b.name.toString()),
     },
+    {
+      title: "Ações",
+      dataIndex: "actions",
+      className: userData.isAdmin ? "" : "hidden",
+      render: (_, record: { key: string }) => (
+        <Button
+          type="primary"
+          danger
+          onClick={() => {
+            handleDelete(record.key);
+          }}
+        >
+          Deletar
+        </Button>
+      ),
+    },
   ];
 
   const unitsData =
-    data?.map((unit) => {
+    units?.map((unit) => {
       return {
         key: unit.id,
         id: unit.id,
         name: unit.name,
       };
     }) ?? [];
+
+  useEffect(() => {
+    if (data) {
+      setUnits(data);
+    }
+  }, [data]);
 
   return (
     <>
@@ -42,10 +78,16 @@ const UnitsModule = (): JSX.Element => {
         </LoadingWrapper>
       ) : (
         <Container>
+          {userData.isAdmin && <ActionHeader userData={userData} />}
           <SortingTable
             data={unitsData}
             columns={columns}
-            pagination={{ pageSize: 20, position: ["topCenter"], style: {backgroundColor: blue[0], margin: 0, padding: "10px 0"} }}
+            pagination={{
+              pageSize: 20,
+              position: ["bottomCenter"],
+              style: { backgroundColor: blue[0], margin: 0, padding: "10px 0" },
+              hideOnSinglePage: true,
+            }}
           />
         </Container>
       )}
---
+++ src/modules/DashboardModules/UnitsModule/type.ts
@@ -0,0 +1,5 @@
+import { type IUser } from 'services/users/types';
+
+export interface UnitsModuleProps {
+  userData: IUser;
+}
