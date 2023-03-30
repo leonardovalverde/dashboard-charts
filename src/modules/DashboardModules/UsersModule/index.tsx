9ebfaba77b0e9f5b2e1e347b5b9cdaff86e759b2
Leonardo Valverde <leovmsi@hotmail.com>
2023-03-30 00:04:33 -0300

    feat: create action to delete user

--- src/modules/DashboardModules/UsersModule/index.tsx
+++ src/modules/DashboardModules/UsersModule/index.tsx
@@ -1,31 +1,38 @@
-import { useEffect,useState } from "react";
-import { Spin } from "antd";
+import { useEffect, useState } from "react";
+import { Button, Spin } from "antd";
 import { type ColumnsType } from "antd/es/table";
 import SortingTable from "components/Table/SortingTable/SortingTable";
 import { useGetAllUnitsQuery } from "services/units/units";
 import { type IUser } from "services/users/types";
-import { useGetAllUsersQuery } from "services/users/users";
+import {
+  useDeleteUserByIdMutation,
+  useGetAllUsersQuery,
+} from "services/users/users";
 
 import { blue } from "@ant-design/colors";
 
 import { getUsersOfSameCompany } from "../utils/functions";
 
+import ActionHeader from "./components/ActionHeader/ActionHeader";
 import { Container, LoadingWrapper } from "./styles";
 import { type UsersModuleProps } from "./types";
 
 const UsersModule = ({ userData }: UsersModuleProps): JSX.Element => {
   const [users, setUsers] = useState<IUser[]>([]);
+  const [updatePost] = useDeleteUserByIdMutation();
   const { data, isLoading } = useGetAllUsersQuery();
-  const {
-    data: dataUnit,
-    isLoading: isLoadingUnit,
-  } = useGetAllUnitsQuery();
+  const { data: dataUnit, isLoading: isLoadingUnit } = useGetAllUnitsQuery();
 
   useEffect(() => {
     if (data) {
       setUsers(getUsersOfSameCompany(userData.companyId, data));
     }
   }, [data, userData.companyId]);
+
+  const handleDelete = (key: string): void => {
+    void updatePost(key).unwrap();
+    setUsers((prev) => prev.filter((item) => item.id !== parseInt(key)));
+  };
 
   const columns: ColumnsType<any> = [
     {
@@ -45,6 +52,22 @@ const UsersModule = ({ userData }: UsersModuleProps): JSX.Element => {
       dataIndex: "unit",
       defaultSortOrder: "descend",
       sorter: (a, b) => a.unit.toString().localeCompare(b.unit.toString()),
+    },
+    {
+      title: "Ações",
+      dataIndex: "actions",
+      className: userData.isAdmin ? "" : "hidden",
+      render: (_, record: { key: string }) => (
+        <Button
+          type="primary"
+          danger
+          onClick={() => {
+            handleDelete(record.key);
+          }}
+        >
+          Deletar
+        </Button>
+      ),
     },
   ];
 
@@ -66,13 +89,15 @@ const UsersModule = ({ userData }: UsersModuleProps): JSX.Element => {
         </LoadingWrapper>
       ) : (
         <Container>
+          {userData.isAdmin && <ActionHeader userData={userData} />}
           <SortingTable
             columns={columns}
             data={usersData}
             pagination={{
               pageSize: 20,
-              position: ["topCenter"],
+              position: ["bottomCenter"],
               style: { backgroundColor: blue[0], margin: 0, padding: "10px 0" },
+              hideOnSinglePage: true,
             }}
           />
         </Container>
